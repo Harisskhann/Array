8138800b6e934c8bce9386e52fa96ef43526d457
M.Haris Khan <hkhan@cedrus.com>
2023-07-20 13:15:20 +0500

    Feat;Array Even Nums

---
+++ Arrayy/Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+
+            Console.WriteLine("Enter 10 integers:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+               
+            }
+            for (int i = 0; i <= numbers.Length-1; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
